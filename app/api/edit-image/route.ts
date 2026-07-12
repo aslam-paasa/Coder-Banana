ad749ad6d6cee0e30bd1b313a8084bf2d41bf72b
Mohammad Aslam <aslampaasa420@gmail.com>
2026-07-12 14:31:58 +0530

    Image Generation Overview & LLM Setup

--- app/api/edit-image/route.ts
+++ app/api/edit-image/route.ts
@@ -1,9 +1,49 @@
 import { NextResponse } from "next/server";
+import { GoogleGenAI } from "@google/genai";
+import fs from 'node:fs';
+
+
+function getMimeType(dataUrl: string): string {
+    const match = dataUrl.match(/^data:(image\/[a-zA-Z+]+);base64,/)
+    return match ? match[1] : 'image/png';
+}
+
+function cleanBase64Image(dataUrl: string): string {
+    return dataUrl.replace(/^data:(.*);base64,/, "")
+}
 
 export async function POST(request: Request) {
     const { imageBase64, prompt } = await request.json()
 
-    // todo: send the image and prompt to model and send back generated image to user.
+    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    const parts = [
+        { text: prompt },
+        {
+            inlineData: {
+                mimeType: getMimeType(imageBase64),
+                data: cleanBase64Image(imageBase64),
+            },
+        }
+    ];
 
-    return NextResponse.json({ imageBase64, prompt })
+
+    const response = await ai.models.generateContent({
+        model: "gemini-3.1-flash-image",
+        contents: parts,
+    });
+
+    const content = response.candidates?.[0].content
+
+    if (content?.parts) {
+        for (const part of content?.parts) {
+            if (part.text) {
+                console.log(part.text);
+            } else if (part.inlineData) {
+                const imageData = part.inlineData.data;
+                return NextResponse.json({ result: imageData })
+            }
+        }
+    }
+    
+    return NextResponse.json({ message: "Failed to generate the image" })
 } 
