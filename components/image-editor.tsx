6ae61919222b139d16b4570d8468d3c0dba4e210
Mohammad Aslam <aslampaasa420@gmail.com>
2026-08-01 21:55:18 +0530

    Load Image to the Canvas

--- components/image-editor.tsx
+++ components/image-editor.tsx
@@ -1,17 +1,52 @@
+import { useCallback, useEffect, useRef } from 'react';
 import { useEditorStore } from '@/store/useEditorState';
-import Image from 'next/image';
-import { useRef } from 'react';
+import NextImage from 'next/image';
 
 const ImageEditor = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const { image } = useEditorStore();
 
+    const draw = useCallback(() => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const ctx = canvas.getContext("2d");
+        if (!ctx) return;
+
+        if (!image) return;
+        const img = new Image();
+        img.src = image;
+
+        img.onload = () => {
+            /* set canvas size to image size */
+            canvas.width = img.naturalWidth;
+            canvas.height = img.naturalHeight;
+
+            /* clear the canvas */
+            ctx.clearRect(0, 0, canvas!.width, canvas!.height);
+            
+            /* draw the image */
+            ctx.drawImage(img, 0, 0);
+        }
+    }, [image])
+
+    useEffect(() => {
+        if (!image) return;
+
+        const img = new Image();
+        img.src = image;
+
+        img.onload = () => {
+            draw();
+        }
+    }, [image, draw])
+
     return (
         <div className='w-full h-full flex items-center justify-center'>
             <canvas
                 ref={canvasRef}
-                className='border border-red-400 max-w-full max-h-full'></canvas>
-            {/* <Image
+                className='max-w-full max-h-full'></canvas>
+            {/* <NextImage
                 width={500}
                 height={500}
                 src={image as string}
